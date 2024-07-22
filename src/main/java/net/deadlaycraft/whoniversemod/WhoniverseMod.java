60350ee6f1e00e94aa06a3d7c50c6909c0436d50
deadlaycraft <bendajtanner@gmail.com>
2024-07-22 01:34:15 +0100

    reverted previous unnecessary changes.

--- src/main/java/net/deadlaycraft/whoniversemod/WhoniverseMod.java
+++ src/main/java/net/deadlaycraft/whoniversemod/WhoniverseMod.java
@@ -19,8 +19,6 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(WhoniverseMod.MOD_ID)
 public class WhoniverseMod {
-    // ELLO ELLO!
-
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "whoniversemod";
     // Directly reference a slf4j logger

17f5dc0fee214112f4b319a83f10ff1a14ad5e81
deadlaycraft <bendajtanner@gmail.com>
2024-07-26 20:34:38 +0100

    - retextured stygian log - retextured stygian leaves - made gallifreyan grass turn into gallifreyan dirt when a block is placed over it, rather than normal dirt - made stygian leaves a LeavesBlock instance - made Redrite Block a PoweredBlock instance - made Redrite Wire a RedStoneWireBlock instance

---
+++ src/main/java/net/deadlaycraft/whoniversemod/block/GallifreyanGrassBlock.java
@@ -0,0 +1,51 @@
+package net.deadlaycraft.whoniversemod.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.GrassBlock;
+import net.minecraft.world.level.block.SnowLayerBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.lighting.LightEngine;
+
+import java.util.Iterator;
+
+public class GallifreyanGrassBlock extends GrassBlock {
+
+
+    public GallifreyanGrassBlock(Properties p_53685_) {
+        super(p_53685_);
+    }
+
+    private static boolean canBeGrass(BlockState pState, LevelReader pLevelReader, BlockPos pPos) {
+        BlockPos blockpos = pPos.above();
+        BlockState blockstate = pLevelReader.getBlockState(blockpos);
+        if (blockstate.is(Blocks.SNOW) && (Integer)blockstate.getValue(SnowLayerBlock.LAYERS) == 1) {
+            return true;
+        } else if (blockstate.getFluidState().getAmount() == 8) {
+            return false;
+        } else {
+            int i = LightEngine.getLightBlockInto(pLevelReader, pState, pPos, blockstate, blockpos, Direction.UP, blockstate.getLightBlock(pLevelReader, blockpos));
+            return i < pLevelReader.getMaxLightLevel();
+        }
+    }
+
+
+    @Override
+    protected void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
+        if (!canBeGrass(pState, pLevel, pPos)) {
+            if (!pLevel.isAreaLoaded(pPos, 1)) {
+                return;
+            }
+
+            pLevel.setBlockAndUpdate(pPos, ModBlocks.GALLIFREYAN_SOIL.get().defaultBlockState());
+        } else {
+            if (!pLevel.isAreaLoaded(pPos, 3)) {
+                return;
+            }
+        }
+    }
+}
--- src/main/java/net/deadlaycraft/whoniversemod/block/ModBlocks.java
+++ src/main/java/net/deadlaycraft/whoniversemod/block/ModBlocks.java
@@ -4,27 +4,36 @@ import net.deadlaycraft.whoniversemod.WhoniverseMod;
 import net.deadlaycraft.whoniversemod.item.ModItems;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraft.world.item.BlockItem;
 
+import java.util.Iterator;
 import java.util.function.Supplier;
 
-public class ModBlocks {
+public class ModBlocks extends Blocks{
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, WhoniverseMod.MOD_ID);
+    private BlockState defaultBlockState;
+
+    //wood logs
+    public static Block log(MapColor pTopMapColor, MapColor pSideMapColor) {
+        return new RotatedPillarBlock(BlockBehaviour.Properties.of().mapColor((p_152624_) -> {
+            return p_152624_.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? pTopMapColor : pSideMapColor;
+        }).instrument(NoteBlockInstrument.BASS).strength(2.0F).sound(SoundType.WOOD).ignitedByLava());
+    }
 
     public static final RegistryObject<Block> REDRITE_WIRE = registerBlock("redrite_wire",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_WIRE))); // a field which assigns itself a value which is the result of a method
+            () -> new RedStoneWireBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_WIRE))); // a field which assigns itself a value which is the result of a method
     public static final RegistryObject<Block> REDRITE_BLOCK = registerBlock("redrite_block",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_BLOCK)));
+            () -> new PoweredBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_BLOCK)));
     public static final RegistryObject<Block> SAFFRITE_BLOCK = registerBlock("saffrite_block",
             () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK)));
     public static final RegistryObject<Block> SOLITITE = registerBlock("solitite",
@@ -32,11 +41,11 @@ public class ModBlocks {
     public static final RegistryObject<Block> GALLIFREYAN_SOIL = registerBlock("gallifreyan_soil",
             () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.DIRT)));
     public static final RegistryObject<Block> GALLIFREYAN_GRASS_BLOCK = registerBlock("gallifreyan_grass_block",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.GRASS_BLOCK)));
+            () -> new GallifreyanGrassBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.GRASS_BLOCK)));
     public static final RegistryObject<Block> STYGIAN_OAK_LOG = registerBlock("stygian_oak_log",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.DARK_OAK_PLANKS)));
+                () -> log(MapColor.COLOR_BROWN, MapColor.COLOR_BROWN));
     public static final RegistryObject<Block> STYGIAN_LEAVES = registerBlock("stygian_leaves",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.DARK_OAK_LEAVES)));
+            () -> new LeavesBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.DARK_OAK_LEAVES)));
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
@@ -55,4 +64,5 @@ public class ModBlocks {
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
+
 }
